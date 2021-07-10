40e5f8f52868be51f6b5d4dd99dca6593b94fc5a
migue <cumplidomiguelangel@gmail.com>
2021-07-10 12:36:34 +0200

    Menu Basico

--- src/StartOperaciones.java
+++ src/StartOperaciones.java
@@ -7,30 +7,39 @@ public class StartOperaciones {
 
 		//COMIENZO DEL PROYECTO
 		
-		//SUMA
 		
-		int num1, num2, resultadoSuma, resultadoResta, resultadoMultiplicacion, resultadoDivision;
+		int num1, num2, resultadoSuma, resultadoResta, resultadoMultiplicacion, resultadoDivision, opcion=0;
+		
 		Scanner sc = new Scanner (System.in);
 		
-		System.out.print("Introduce el primer número para sumar: ");
+		System.out.print("Introduce el primer número: ");
 		num1 = sc.nextInt();
 		
-		System.out.print("Introduce el segundo número para sumar: ");
+		System.out.print("Introduce el segundo número: ");
 		num2 = sc.nextInt();
 		
-		resultadoSuma=num1+num2;
+		System.out.println("");
+		System.out.print("---MENÚ---");
+		opcion=sc.nextInt();
+		System.out.println("");
+		System.out.println("1) Suma");
+		System.out.println("2) Resta");
+		System.out.println("3) Multiplicación");
+		System.out.println("4) División");
+		System.out.println("");
+		System.out.print("Elija una opción: ");
 
+		//SUMA
+		resultadoSuma=num1+num2;
 		System.out.println("El restultado de "+num1+ " + "+num2+" es: "+resultadoSuma);
 		
 		//RESTA
 		resultadoResta=num1-num2;
 		System.out.println("El restultado de "+num1+ " - "+num2+" es: "+resultadoResta);
 
-		
 		//Multiplicacion
 		resultadoMultiplicacion=num1*num2;
 		System.out.println("El restultado de "+num1+ " * "+num2+" es: "+resultadoMultiplicacion);
-
 		
 		//Division
 		resultadoDivision=num1/num2;
