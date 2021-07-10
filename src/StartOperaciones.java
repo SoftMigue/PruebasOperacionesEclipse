9850c45808c1406e8f4342ba5a5368dd409b1fc6
migue <cumplidomiguelangel@gmail.com>
2021-07-10 12:29:48 +0200

    Mostrar todas las operaciones

--- src/StartOperaciones.java
+++ src/StartOperaciones.java
@@ -7,9 +7,9 @@ public class StartOperaciones {
 
 		//COMIENZO DEL PROYECTO
 		
-		//SUMAS
+		//SUMA
 		
-		int num1, num2, resultado;
+		int num1, num2, resultadoSuma, resultadoResta, resultadoMultiplicacion, resultadoDivision;
 		Scanner sc = new Scanner (System.in);
 		
 		System.out.print("Introduce el primer número para sumar: ");
@@ -18,9 +18,26 @@ public class StartOperaciones {
 		System.out.print("Introduce el segundo número para sumar: ");
 		num2 = sc.nextInt();
 		
-		resultado=num1+num2;
+		resultadoSuma=num1+num2;
 
-		System.out.println("El restultado de "+num1+ " + "+num2+" es: "+resultado);
+		System.out.println("El restultado de "+num1+ " + "+num2+" es: "+resultadoSuma);
+		
+		//RESTA
+		resultadoResta=num1-num2;
+		System.out.println("El restultado de "+num1+ " - "+num2+" es: "+resultadoResta);
+
+		
+		//Multiplicacion
+		resultadoMultiplicacion=num1*num2;
+		System.out.println("El restultado de "+num1+ " * "+num2+" es: "+resultadoMultiplicacion);
+
+		
+		//Division
+		resultadoDivision=num1/num2;
+		System.out.println("El restultado de "+num1+ " / "+num2+" es: "+resultadoDivision);
+
+		
+		
 		
 	}
 
