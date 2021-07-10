6a48c3fe8b04257bc98891ca0ab57ba51aae1672
migue <cumplidomiguelangel@gmail.com>
2021-07-10 13:12:16 +0200

    Todos los menus finalizados

--- src/StartOperaciones.java
+++ src/StartOperaciones.java
@@ -5,55 +5,67 @@ public class StartOperaciones {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//COMIENZO DEL PROYECTO
-		
-		
-		int num1, num2, resultadoSuma, resultadoResta, resultadoMultiplicacion, resultadoDivision, opcion=0;
-		
-		Scanner sc = new Scanner (System.in);
-		
+		// COMIENZO DEL PROYECTO
+
+		int num1, num2, resultadoSuma, resultadoResta, resultadoMultiplicacion, resultadoDivision, opcion = 0;
+
+		Scanner sc = new Scanner(System.in);
+
 		System.out.print("Introduce el primer número: ");
 		num1 = sc.nextInt();
-		
+
 		System.out.print("Introduce el segundo número: ");
 		num2 = sc.nextInt();
-		
-		System.out.println("");
-		System.out.print("---MENÚ---");
-		System.out.println("");
-		System.out.println("1) Suma");
-		System.out.println("2) Resta");
-		System.out.println("3) Multiplicación");
-		System.out.println("4) División");
-		System.out.println("");
-		System.out.print("Elija una opción: ");
-		opcion=sc.nextInt();
-		
-		if (opcion==1) {
-			//SUMA
-			resultadoSuma=num1+num2;
-			System.out.println("El restultado de "+num1+ " + "+num2+" es: "+resultadoSuma);
-		}
 
-		if (opcion==2) {
-			//RESTA
-			resultadoResta=num1-num2;
-			System.out.println("El restultado de "+num1+ " - "+num2+" es: "+resultadoResta);
-		}
-		
-		if (opcion==3) {
-			//Multiplicacion
-			resultadoMultiplicacion=num1*num2;
-			System.out.println("El restultado de "+num1+ " * "+num2+" es: "+resultadoMultiplicacion);
-		}
-		
-		if (opcion==4) {
-			//Division
-			resultadoDivision=num1/num2;
-			System.out.println("El restultado de "+num1+ " / "+num2+" es: "+resultadoDivision);
-		}
-		
-		
+		do {
+			System.out.println("");
+			System.out.print("---MENÚ---");
+			System.out.println("");
+			System.out.println("1) Suma");
+			System.out.println("2) Resta");
+			System.out.println("3) Multiplicación");
+			System.out.println("4) División");
+			System.out.println("0) Salir");
+			System.out.println("");
+
+			System.out.print("Elija una opción: ");
+			System.out.println("");
+			opcion = sc.nextInt();
+
+			if (opcion <= 4 && opcion >= 1) {
+
+				if (opcion == 1) {
+					// SUMA
+					resultadoSuma = num1 + num2;
+					System.out.println("El restultado de " + num1 + " + " + num2 + " es: " + resultadoSuma);
+				}
+
+				if (opcion == 2) {
+					// RESTA
+					resultadoResta = num1 - num2;
+					System.out.println("El restultado de " + num1 + " - " + num2 + " es: " + resultadoResta);
+				}
+
+				if (opcion == 3) {
+					// Multiplicacion
+					resultadoMultiplicacion = num1 * num2;
+					System.out.println("El restultado de " + num1 + " * " + num2 + " es: " + resultadoMultiplicacion);
+				}
+
+				if (opcion == 4) {
+					// Division
+					resultadoDivision = num1 / num2;
+					System.out.println("El restultado de " + num1 + " / " + num2 + " es: " + resultadoDivision);
+				}
+			}
+			else if (opcion!=0){
+				System.out.println("Introduce una opción válida");
+			}
+
+		} while (opcion != 0);
+
+		System.out.println("---Fin del programa---");
+
 	}
 
 }
